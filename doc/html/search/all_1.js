4ec30655bee846de18ab98b8679c47bf99750ea2
tristan Strahler <tristan.strahler@hotmail.de>
2019-04-02 17:00:56 +0200

    .gitignore fix

--- doc/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['loadfile',['loadFile',['../canflash_8h.html#a9284dbafa55fb0f7a9133ed784f55d11',1,'loadFile(char *filePath, uint8_t **fileBuffer, uint32_t *fileLen):&#160;canflash.c'],['../canflash_8c.html#a9284dbafa55fb0f7a9133ed784f55d11',1,'loadFile(char *filePath, uint8_t **fileBuffer, uint32_t *fileLen):&#160;canflash.c']]]
-];
